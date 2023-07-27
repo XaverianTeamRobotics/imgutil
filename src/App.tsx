066a334ff7b7e0eeb6d1302af4d3332b1e3e0c5c
Thomas Ricci <git@thomasricci.dev>
2023-07-27 12:04:32 -0400

    bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ interface Props {
 export const App: FC<Props> = (): ReactElement => {
   return (
     <React.Fragment>
-
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </React.Fragment>
   );
 };
---
+++ src/Image.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactElement } from "react";
+
+interface Props {
+  url: string;
+}
+
+export const Image: FC<Props> = ({ url }): ReactElement => {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+};
